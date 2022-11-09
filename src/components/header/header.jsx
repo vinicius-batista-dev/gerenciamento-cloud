8419764c1610d4ff86e7731eca46d3ecc460e565
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-11-09 15:56:12 -0300

    atualizando o projeto

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,44 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  //Ao clicar no botao de construcao service tera um alerta para o usuario logar
+  if (window.location.pathname === "/construcaoService") {
+    alert("Por favor, deve estar logado");
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/signin");
+  };
+
   return (
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="/signin">
-            Management System
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item"></li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/construcaoService">
-                  Cadastrar
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/listarConstrucao">
-                  Consultar
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          <span className="ms-2">Bem Vindo</span>
+        </Link>
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link active">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/construcaoService" className="nav-link">
+              Service
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/listarConstrucao" className="nav-link">
+              ListarConstrucao
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link onClick={handleLogout} to="/signin" className="nav-link">
+              Sair
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 

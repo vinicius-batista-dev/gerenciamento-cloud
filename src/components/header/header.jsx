6dc1e5615479c9b78c6b77fe8219ab46c3a8eef8
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-11-10 14:43:44 -0300

    atualizando a navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -3,43 +3,40 @@ import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
+  const [search, setSearch] = React.useState("");
+
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const logout = () => {
     localStorage.removeItem("token");
-    navigate("/signin");
+    navigate("/");
   };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
-          Gerencia
+          Bem Vindo
         </Link>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/construcaoService">
-                Serviços
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/listarConstrucao">
-                Consulta
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/signin" onClick={handleLogout}>
-                Sair
-              </Link>
-            </li>
-          </ul>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link active" aria-current="page" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/construcaoService">
+              Construcao Service
+            </Link>
+            <Link className="nav-link" to="/listarConstrucao">
+              Listar Construcao
+            </Link>
+            <Link onClick={logout} className="nav-link" to="/">
+              Sair
+            </Link>
+          </div>
         </div>
       </div>
     </nav>

e4d68730e6caa63cf37ef0c4b803c87222b0873f
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-11-10 22:01:12 -0300

    atualizando a logo

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -31,7 +31,7 @@ const Header = () => {
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
               <Link className="navbar-brand" to="/">
-                Gerenciador de Tarefas
+                MSYS
               </Link>
               <button
                 onChange={handleToggle}

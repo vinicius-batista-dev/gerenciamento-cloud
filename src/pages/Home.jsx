d5fb11e073bd2e288d4c28815b31197cfdf0a149
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-12-01 14:35:56 -0300

    fix atualizando a home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,96 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Link, redirect, useNavigation } from "react-router-dom";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import { Button, Typography } from "@mui/material";
 
 const Home = () => {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-12">
-          <h1>Bem Vindo</h1>
-          <p>
-            Seja bem vindo ao nossa API.
-          </p>
+        <div className="col-md-12">
+          <div className="card">
+            <div className="card-header">
+              <h3>Servicos Realizados</h3>
+            </div>
+            <div className="card-body">
+              <div className="row">
+                <div className="col-md-4">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="https://diariodorio.com/wp-content/uploads/2019/08/Antenas-no-Sumar%C3%A9.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Antenas
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Construção de casas, apartamentos, prédios, etc.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Ver</Button>
+                    </CardActions>
+                  </Card>
+                </div>
+                <div className="col-md-4">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="https://www.ryazbek.com.br/wp-content/uploads/2019/11/original-ccba23ab2eb493b23837674485286bcf.jpg"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Apartamento
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Construção de casas, apartamentos, prédios, etc.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Ver</Button>
+                    </CardActions>
+                  </Card>
+                </div>
+                <div className="col-md-4">
+                  <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                      component="img"
+                      height="140"
+                      image="https://viagemeturismo.abril.com.br/wp-content/uploads/2016/10/park-017-1.jpeg?quality=70&strip=info"
+                      alt="green iguana"
+                    />
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        Jardins
+                      </Typography>
+                      <Typography variant="body2" color="text.secondary">
+                        Construção de casas, apartamentos, prédios, etc.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Ver</Button>
+                    </CardActions>
+                  </Card>
+                </div>
+              </div>
+            </div>
+            <p className="text-center">
+              <Link to="/listarConstrucao" className="btn btn-primary">
+                Ver todos
+              </Link>
+            </p>
+          </div>
         </div>
-        <p>
-          <Link to="/signin">Testar Sistema</Link>
-        </p>
-      </div>  
+      </div>
     </div>
   );
 };

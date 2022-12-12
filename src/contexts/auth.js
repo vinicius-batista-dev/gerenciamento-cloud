9031497b137d3c0b7000b1523a78195b685b553c
vinicius-batista-dev <20201012000256@pucgo.edu.br>
2022-12-12 01:02:57 -0300

    atualizando o auth context

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,29 +1,17 @@
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
-  const [loading, setLoading] = useState(true);
 
   if (token) {
     localStorage.setItem("token", token);
   }
 
-  useEffect(() => {
-    const recoveredToken = localStorage.getItem("token");
-
-    if (recoveredToken) {
-      setToken(recoveredToken);
-    }
-
-    setLoading(false);
-  }, []);
-
   return (
     <AuthContext.Provider
       value={{
-        loading,
         token,
         setToken,
       }}
